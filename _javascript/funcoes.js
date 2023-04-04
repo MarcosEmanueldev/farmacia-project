9301606c48598c622c2673f193ab909718257918
Mateus Macedo <mateusemanuel198@gmail.com>
2023-04-03 21:45:31 -0300

    Initial commit

---
+++ _javascript/funcoes.js
@@ -0,0 +1,18 @@
+function mudaFoto1(foto){
+  document.getElementById("icone1").src = foto;
+}
+function mudaFoto2(foto){
+  document.getElementById("icone2").src = foto;
+}
+function mudaFoto3(foto){
+  document.getElementById("icone3").src = foto;
+}
+function mudaFoto4(foto){
+  document.getElementById("icone4").src = foto;
+}
+function mudaFoto5(foto){
+  document.getElementById("icone5").src = foto;
+}
+function mudaFoto6(foto){
+  document.getElementById("icone6").src = foto;
+}
